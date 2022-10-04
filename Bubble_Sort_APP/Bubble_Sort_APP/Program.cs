7efca54cd1753cae54ae8fd13e8e227328bf8cf0
Cesar Mello <cesarpcj@hotmail.com>
2022-10-04 13:23:59 +0100

    first commit to dev

---
+++ Bubble_Sort_APP/Bubble_Sort_APP/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Bubble_Sort_APP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static void BubleSort(int[] numList)
+        {
+
+        }
+    }
+}
---
+++ Bubble_Sort_APP/Bubble_Sort_Tests/Tests.cs
@@ -0,0 +1,14 @@
+using Bubble_Sort_APP;
+namespace Bubble_Sort_Tests
+{
+    public class Tests
+    {
+        
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
